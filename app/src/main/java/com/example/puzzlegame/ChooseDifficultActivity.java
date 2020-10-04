7cb9a201836c45eacb30d19eaf68f2b802657ed0
galtager <galtager@hotmail.com>
2020-10-04 10:28:04 +0300

    Add Some Features

--- app/src/main/java/com/example/puzzlegame/ChooseDifficultActivity.java
+++ app/src/main/java/com/example/puzzlegame/ChooseDifficultActivity.java
@@ -9,11 +9,8 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Objects;
 
 public class ChooseDifficultActivity extends AppCompatActivity {
     
--- app/src/main/java/com/example/puzzlegame/Home.java
+++ app/src/main/java/com/example/puzzlegame/Home.java
@@ -2,9 +2,14 @@ package com.example.puzzlegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -14,6 +19,9 @@ import android.widget.ImageButton;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
+import com.google.android.material.chip.Chip;
+
+import java.util.Locale;
 
 public class Home extends AppCompatActivity {
 
@@ -21,7 +29,9 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
         Button startButton = findViewById(R.id.start_button);
+        Button settingbutton = findViewById(R.id.setting_button);
         ImageButton facebook =findViewById(R.id.facebook);
         ImageButton instagram =findViewById(R.id.instagram);
         ImageButton github =findViewById(R.id.github);
@@ -29,6 +39,8 @@ public class Home extends AppCompatActivity {
         github.setOnClickListener(onClickListener);
         instagram.setOnClickListener(onClickListener);
         facebook.setOnClickListener(onClickListener);
+        settingbutton.setOnClickListener(onClickListener);
+        startButton.setOnClickListener(onClickListener);
 
 
 
@@ -36,15 +48,6 @@ public class Home extends AppCompatActivity {
                 .duration(3000)
                 .repeat(2)
                 .playOn(startButton);
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent chooseDifficultIntent = new Intent(Home.this, ChooseDifficultActivity.class);
-                startActivity(chooseDifficultIntent);
-                overridePendingTransition(R.anim.slide_out_left, R.anim.slide_in_right);
-            }
-        });
-
 
     }
     View.OnClickListener onClickListener = new View.OnClickListener() {
@@ -64,10 +67,58 @@ public class Home extends AppCompatActivity {
                     browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Galtager/PuzzleGame"));
                     startActivity(browserIntent);
                     break;
-
-
-
+                case R.id.setting_button:
+                    openDialog();
+                    break;
+                case R.id.start_button:
+                    Intent chooseDifficultIntent = new Intent(Home.this, ChooseDifficultActivity.class);
+                    startActivity(chooseDifficultIntent);
+                    overridePendingTransition(R.anim.slide_out_left, R.anim.slide_in_right);
+                    break;
             }
         }
     };
+    public void openDialog(){
+        final Dialog dialog = new Dialog(Home.this);
+        dialog.setContentView(R.layout.dialog_setting);
+        dialog.show();
+        Chip hebrewChip = dialog.findViewById(R.id.hebrewChip);
+        Chip englishChip = dialog.findViewById(R.id.englishChip);
+
+        hebrewChip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setAppLocale("iw");
+                dialog.dismiss();
+                restartActivity();
+            }
+        });
+        englishChip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setAppLocale("en");
+                dialog.dismiss();
+                restartActivity();
+            }
+        });
+
+    }
+    private void setAppLocale(String localeCode){
+        Resources resources = getResources();
+        DisplayMetrics displayMetrics = resources.getDisplayMetrics();
+        Configuration configuration = resources.getConfiguration();
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.JELLY_BEAN_MR1){
+            configuration.setLocale(new Locale(localeCode.toLowerCase()));
+        }
+        else {
+            configuration.locale = new Locale(localeCode.toLowerCase());
+        }
+        resources.updateConfiguration(configuration,displayMetrics);
+    }
+    private void restartActivity()
+    {
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
+    }
 }
